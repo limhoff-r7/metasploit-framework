8a0b8905a59d393554b5655bd01b99502fa430a7
Luke Imhoff <luke_imhoff@rapid7.com>
2014-02-18 15:06:07 -0600

    Allow empty String for shellcode data for assembled payloads.
    MSP-9228

--- app/models/metasploit/framework/payload/assembled.rb
+++ app/models/metasploit/framework/payload/assembled.rb
@@ -8,7 +8,7 @@ class Metasploit::Framework::Payload::Assembled < Metasploit::Model::Base
   #
   #   @return [String] {Metasm::EncodedData#data} or a plain `String` for pre-assembled payloads declared in the payload
   #     module.
-  attr_accessor :data
+  attr_writer :data
 
   # @!attribute [rw] offset_relative_address_and_type_by_name
   #   @note DO NOT include 'RAW' type offsets in this list as this list should be valid for usage
@@ -18,17 +18,16 @@ class Metasploit::Framework::Payload::Assembled < Metasploit::Model::Base
   #   @return [Hash{String => Array<(Integer, String)>}]
   attr_writer :offset_relative_address_and_type_by_name
 
-
-  #
-  # Validations
-  #
-
-  validates :data,
-            presence: true
-
   #
   # Methods
   #
+
+  # Data can be empty for shellcode-less payloads like 'cmd/unix/interact'.
+  #
+  # @return [String] ''
+  def data
+    @data ||= ''
+  end
 
   def offset_relative_address_and_type_by_name
     @offset_relative_address_and_type_by_name ||= {}
