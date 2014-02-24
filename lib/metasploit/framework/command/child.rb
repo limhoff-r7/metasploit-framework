2e6106873d88b9f1f42a55e3383d1335a2f7df61
Luke Imhoff <luke_imhoff@rapid7.com>
2014-02-24 16:26:53 -0600

    Metasploit::Framework::Command::Child
    MSP-9228
    
    Extract Metasploit::Framework::Command::Child from
    Metasploit::Framework::Console:Command::Child for pieces shared between
    console and command line.

---
+++ lib/metasploit/framework/command/child.rb
@@ -0,0 +1,44 @@
+module Metasploit::Framework::Command::Child
+  extend ActiveSupport::Concern
+
+  included do
+    include ActiveModel::Validations
+
+    #
+    # Validations
+    #
+
+    validates :parent,
+              presence: true
+
+    # Undefined writers as they won't be read back out by the readers because those delegate to #parent.  Writers
+    # aren't delegated to #parent because it needs to be clear that the #parent is responsible for parsing the words
+    undef_method :words=
+  end
+
+  #
+  # Attributes
+  #
+
+  # @!attribute [rw] parent
+  #   The parent command of which this command is a subcommand.
+  #
+  #   @return [Metasploit::Framework::Console::Command::Parent]
+  attr_accessor :parent
+
+  #
+  # Methods
+  #
+
+  # Words from {#parent}.
+  #
+  # @return [Array<String>] from {#parent}
+  # @return [[]] if {#parent} is nil
+  def words
+    unless parent.nil?
+      parent.words
+    else
+      []
+    end
+  end
+end
--- lib/metasploit/framework/console/command/child.rb
+++ lib/metasploit/framework/console/command/child.rb
@@ -2,31 +2,13 @@
 module Metasploit::Framework::Console::Command::Child
   extend ActiveSupport::Concern
 
+  include Metasploit::Framework::Command::Child
+
   included do
-    include ActiveModel::Validations
-
-    #
-    # Validations
-    #
-
-    validates :parent,
-              presence: true
-
     # Undefined writers as they won't be read back out by the readers because those delegate to #parent.  Writers
     # aren't delegated to #parent because it needs to be clear that the #parent is responsible for parsing the words
     undef_method :partial_word=
-    undef_method :words=
   end
-
-  #
-  # Attributes
-  #
-
-  # @!attribute [rw] parent
-  #   The parent command of which this command is a subcommand.
-  #
-  #   @return [Metasploit::Framework::Console::Command::Parent]
-  attr_accessor :parent
 
   #
   # Methods
@@ -45,16 +27,4 @@ module Metasploit::Framework::Console::Command::Child
            # must allow nil so that parent can be validated
            allow_nil: true,
            to: :parent
-
-  # Words from {#parent}.
-  #
-  # @return [Array<String>] from {#parent}
-  # @return [[]] if {#parent} is nil
-  def words
-    unless parent.nil?
-      parent.words
-    else
-      []
-    end
-  end
 end
