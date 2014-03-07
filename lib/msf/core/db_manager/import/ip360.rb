8764f1fa70e1cc9986d60502d2d61ab2c5b26314
Luke Imhoff <luke_imhoff@rapid7.com>
2014-03-07 15:39:38 -0600

    Remove unused variables
    MSP-9228

--- lib/msf/core/db_manager/import/ip360.rb
+++ lib/msf/core/db_manager/import/ip360.rb
@@ -76,8 +76,6 @@ module Msf::DBManager::Import::IP360
   #
   def import_ip360_aspl_xml(args={}, &block)
     data = args[:data]
-    wspace = args[:wspace] || workspace
-    bl = validate_ips(args[:blacklist]) ? args[:blacklist].split : []
 
     if not data.index("<ontology")
       raise DBImportError.new("The ASPL file does not appear to be valid or may still be compressed")
