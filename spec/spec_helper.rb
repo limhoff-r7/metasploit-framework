1fbb4c5a7b06235e86add532340f323891976620
Luke Imhoff <luke_imhoff@rapid7.com>
2014-03-18 08:51:42 -0500

    NO_RELOAD for Rakefile and spec_helper
    Get rid of 'already initialized constants' by ensure that eager loading
    uses require from not just command line binaries like msfconsole and
    msfvenom, which use msfenv, but also rake taks and specs run with
    rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 # -*- coding:binary -*-
+# Use require for ActiveSupport::Dependencies since we don't use use the unloading mechanism
+# @todo Use ActiveSupport::Dependencies unloading mechanism to eliminate need to restart msfconsole when developing core code
+# @see https://github.com/rails/rails/blob/64226302d82493d9bf67aa9e4fa52b4e0269ee3d/activesupport/lib/active_support/dependencies.rb#L33
+ENV['NO_RELOAD'] = 'true'
+
 require 'rubygems'
 require 'bundler'
 Bundler.require(:default, :test, :db)
