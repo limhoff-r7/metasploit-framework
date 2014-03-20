4a8d45bd85b03ce912f6dadd89199f91de279b77
Luke Imhoff <luke_imhoff@rapid7.com>
2014-03-20 11:14:22 -0500

    Remove unused delegates in Msf::ModuleSet
    MSP-9228

--- lib/msf/core/module_set.rb
+++ lib/msf/core/module_set.rb
@@ -39,26 +39,6 @@ class Msf::ModuleSet < Metasploit::Model::Base
   # Methods
   #
 
-  # @!method db
-  #   Used to check whether the database is connected.
-  #
-  #   @return (see Msf::Framework#db)
-  delegate :db,
-           to: :framework
-
-  # @!method cache
-  #   The module cache that loads metasploit classes.
-  #
-  #   @return (see Msf::ModuleManager::Cache#cache)
-  #
-  # @!method framework
-  #   Framework that should be notified of events and passed to {#create created instance}.
-  #
-  #   @return (see Msf::ModuleManager#framework)
-  delegate :cache,
-           :framework,
-           to: :module_manager
-
   # Creates a metasploit instanc using the supplied `Mdm::Module::Class#reference_name`.
   # `Mdm::Module::Class#module_type` is assumed to be equal to {#module_type}.
   #
