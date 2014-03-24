575ec47484e4e13a3772078be73b35a21bad3ed8
Luke Imhoff <luke_imhoff@rapid7.com>
2014-03-24 16:29:15 -0500

    Fully-qualify Msf::ServiceState
    MSP-9228
    
    Make sure Msf::ServiceState always uses Msf:: namespace.

--- lib/msf/core/db_manager/service.rb
+++ lib/msf/core/db_manager/service.rb
@@ -78,7 +78,7 @@ module Msf::DBManager::Service
           dlog("Unknown attribute for Service: #{k}")
         end
       }
-      service.state ||= ServiceState::Open
+      service.state ||= Msf::ServiceState::Open
       service.info  ||= ""
 
       if (service and service.changed?)
@@ -123,7 +123,7 @@ module Msf::DBManager::Service
   def services(wspace = workspace, only_up = false, proto = nil, addresses = nil, ports = nil, names = nil)
     with_connection {
       conditions = {}
-      conditions[:state] = [ServiceState::Open] if only_up
+      conditions[:state] = [Msf::ServiceState::Open] if only_up
       conditions[:proto] = proto if proto
       conditions["hosts.address"] = addresses if addresses
       conditions[:port] = ports if ports
